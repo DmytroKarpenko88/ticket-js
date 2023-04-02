cb9d224645f0b4e75e824527452fa436b262e062
Dmytro Karpenko <kardmitriy@gmail.com>
2023-04-02 13:15:11 +0300

    Update api.js

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
+
+
 // const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
 
 //   'https://app.ticketmaster.com/discovery/v2/events.json?apikey=r2n1qFmKAGJf9u4guojkmdnIGBUfuJeA';
 
 const API_KEY = 'r2n1qFmKAGJf9u4guojkmdnIGBUfuJeA';
 const URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
+
+
 
 export async function fetchTicket() {
   const config = {
